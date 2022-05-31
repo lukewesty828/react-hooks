bfcddafafc79e66eec59ebfae9f37e8de8f36f3a
lukewesty828 <lukewesty828@gmail.com>
2022-05-31 11:09:29 -0400

    Epic React Hooks exercises changes from 5/27

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,10 +2,17 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  // Extra credit 1 - preserve state in localStorage (useEffect)
+  // Extra credit 2 - refactor EC1 solution to use useLocalStorageState custom hook 
+
+  const [squares, setSquares] = useLocalStorageState(
+    'squares',
+    Array(9).fill(null),
+  )
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -13,27 +20,29 @@ function Board() {
   // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
-
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
+  // 🦉 It's typically a bad idea to mutate or directly change state in React.
+  // Doing so can lead to subtle bugs that can easily slip into production.
+  // 🐨 make a copy of the squares array
+  // 💰 `[...squares]` will do it!)
+  // 🐨 set the value of the square that was selected
+  // 💰 `squaresCopy[square] = nextValue`
+  // 🐨 set the squares to your copy
   function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+    if (winner || squares[square]) {
+      return
+    }
+    const squaresCopy = [...squares]
+    squaresCopy[square] = nextValue
+    setSquares(squaresCopy)
   }
 
   function restart() {
+    setSquares(Array(9).fill(null))
     // 🐨 reset the squares
     // 💰 `Array(9).fill(null)` will do it!
   }
@@ -48,8 +57,7 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
